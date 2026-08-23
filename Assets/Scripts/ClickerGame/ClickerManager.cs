7f3a243c84ad19aad7a5ed361fb0c4e8b465d61d
Dominic Z <dmnzhang@gmail.com>
2026-08-22 20:56:16 -0400

    saves and calculates offline time

--- Assets/Scripts/ClickerGame/ClickerManager.cs
+++ Assets/Scripts/ClickerGame/ClickerManager.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using TMPro;
+using System.IO;
+using System.Collections.Generic;
+using System;
 
 public class ClickerManager : MonoBehaviour
 {
@@ -7,8 +10,32 @@ public class ClickerManager : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI counterText;
 
+    private void Start() {
+        Debug.Log(CaculateOfflineTime().ToString());
+    }
+
     public void UpdateCounter(int amount) {
         counter += amount;
         counterText.text = counter.ToString();
+    }
+
+    public void SaveGame() {
+        PlayerPrefs.SetString("QuitTime", DateTime.UtcNow.ToString());
+        PlayerPrefs.Save();
+    }
+
+    private double CaculateOfflineTime() {
+        if (!PlayerPrefs.HasKey("QuitTime")) 
+            return 0;
+
+        string quitTime = PlayerPrefs.GetString("QuitTime");
+
+        if (DateTime.TryParse(quitTime, out DateTime lastQuitTime)) {
+            TimeSpan ts = DateTime.UtcNow - lastQuitTime;
+            
+            return ts.TotalSeconds;
+        }
+
+        return 0;
     }
 }
